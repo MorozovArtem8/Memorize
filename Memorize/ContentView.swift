f2ebfdc090b2cd1bed2385fe92d23185854a4414
MorozovArtem8 <Vjhjpjaa32@yandex.ru>
2023-04-10 23:07:35 +0300

    next

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,18 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚌","🛺","🚜","🚗","🚁","🛶","⛵️","🚤","🛥️","🛳️","⛴️","🚢","🛟","⚓️","🪝","⛽️","🚄","🚅","🚈","🚂"]
+    @State var emojiCount = 6
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("fdsfds")
-        }
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+       
+    }
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
         .padding(.horizontal)
-        
-//
+}
+    var remove: some View{
+       Button(action: {emojiCount -= 1},
+              label: {
+           Image(systemName: "minus.circle")
+       })
+   }
+      var add: some View{
+       Button(action: {emojiCount += 1},
+              label: {
+           Image(systemName: "plus.circle")
+       })
+   }
+}
+
+struct CardView: View{
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View{
+         ZStack{
+             let shape =  RoundedRectangle(cornerRadius: 20)
+             if isFaceUp{
+                 shape.stroke(lineWidth: 3)
+                 shape.fill().foregroundColor(.white)
+                 Text(content).font(.largeTitle)
+             } else{
+                 shape.fill()
+            }
+        }
+         
+         .onTapGesture{
+             isFaceUp = !isFaceUp
+         }
     }
 }
+
+
 
 
 
@@ -38,5 +85,9 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+            
     }
 }
